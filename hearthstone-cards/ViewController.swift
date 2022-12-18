32eff101c1cf5f607d0d2dbb4cd6c361f9ad93d3
Willian Pinho <willian.pinho.ext@dasa.com.br>
2022-12-18 10:33:33 -0300

    Initial Commit

---
+++ hearthstone-cards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hearthstone-cards
+//
+//  Created by Willian Junior Peres de Pinho on 18/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
